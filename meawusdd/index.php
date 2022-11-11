d5f1fae0bbd85817b5147b0f3d8f4cb062541683
diewland <diewland@gmail.com>
2022-11-11 22:29:57 +0700

    draw UI

---
+++ meawusdd/index.php
@@ -0,0 +1,33 @@
+<?php $t = time(); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Just Meaw - Public Free Mint Never Die</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="../app.css?t=<?=$t?>" rel="stylesheet">
+  </head>
+  <body class="bg-dark">
+
+    <div class='row'>
+      <div class='col-md-4'></div>
+
+      <div class='col'>
+        <div class="card">
+          <img src="https://qx.app/_next/image?url=https%3A%2F%2Ffanbase-1.s3.amazonaws.com%2Fquixotic-collection-profile%2Fimageedit_4_5517035030.png&w=3840&q=75" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Just Meaw - Public Free Mint Never Die</h5>
+            <p class="card-text">I know we love public free mint. But it is currently unavailable. So Go to website link and fill in the item 6.0 mint token per banner suggest. Mint Token = 0 NumberOfToken(s) = 1 Write New Public Free mint NFT Never die. Enjoy!!!</p>
+            <a href="#/" id="btn-mint" class="btn btn-dark w-100">Mint</a>
+          </div>
+        </div>
+      </div>
+
+      <div class='col-md-4'></div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="../app.js?t=<?=$t?>"></script>
+  </body>
+</html>
