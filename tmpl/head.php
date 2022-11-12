648cc2a6ba766d220376717f4c41572b9ecb69ed
diewland <diewland@gmail.com>
2022-11-12 11:42:18 +0700

    add metadata

--- tmpl/head.php
+++ tmpl/head.php
@@ -1,10 +1,18 @@
 <?php
   $t = time();
   $body_cls = "bg-dark";
+  $url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="<?=htmlentities($title, ENT_QUOTES)?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="<?=$url?>" />
+  <meta property="og:image" content="<?=$img?>" />
+  <meta property="og:description" content="<?=htmlentities($desc, ENT_QUOTES)?>" />
+  <meta name="theme-color" content="#FFFFFF">
+  <meta name="twitter:card" content="<?=$img?>">
   <title><?=$title?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
