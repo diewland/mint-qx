ef624e2e21248749257cac8fa3dbf5b606b6f6aa
diewland <diewland@gmail.com>
2022-12-06 18:07:34 +0700

    update metadata

--- tmpl/head.php
+++ tmpl/head.php
@@ -13,6 +13,13 @@
   <meta property="og:description" content="<?=htmlentities($desc, ENT_QUOTES)?>" />
   <meta name="theme-color" content="#FFFFFF">
   <meta name="twitter:card" content="<?=$img?>">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@diewland_nft">
+  <meta name="twitter:title" content="<?=htmlentities($title, ENT_QUOTES)?>" />
+  <meta name="twitter:description" content="<?=htmlentities($desc, ENT_QUOTES)?>" />
+  <meta name="twitter:creator" content="@diewland_nft">
+  <meta name="twitter:image:src" content="<?=$img?>" />
+  <meta name="twitter:domain" content="diew.app">
   <title><?=$title?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
